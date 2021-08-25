5d916d4d5455771f7ede58bb3c02e802ec62e123
Guta101 <gustavo.tartaglia2@gmail.com>
2021-08-25 14:23:34 -0300

    Bullets + Firing
    - Added firing + color change
    - Added bullet

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    [SerializeField]private Rigidbody2D rb;
+    private int bulletColour;
+    private float bulletSpeed = 10f;
+
+    void Start()
+    {
+        Debug.Log(bulletColour);
+    }
+
+    void FixedUpdate()
+    {
+        rb.velocity = new Vector2(bulletSpeed, rb.velocity.y);
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        Destroy(gameObject);
+    }
+
+    public void SetColour(int bulletColour)
+    {
+        this.bulletColour = bulletColour;
+    }
+
+    public int GetColour()
+    {
+        return bulletColour;
+    }
+}
---
+++ Assets/Scripts/PlayerShoot.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShoot : MonoBehaviour
+{
+    [SerializeField]private Transform firepoint;
+    [SerializeField]private GameObject bulletPrefab;
+    private int bulletColour = 0;
+    private int ammo = 4;
+    private int reloads = 0;
+
+    void Update()
+    {
+        HandleShooting();
+        HandleAmmo();
+    }
+
+    public void AddReload(int reloads)
+    {
+        this.reloads += reloads;
+    }
+
+    public int GetReloads()
+    {
+        return reloads;
+    }
+
+    void HandleShooting()
+    {
+        if(Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        if (ammo > 0)
+        {   
+            //Instantiate the bullet and send it what colour it should be
+            GameObject bulletInstance = Instantiate(bulletPrefab, firepoint.position, firepoint.rotation);
+            bulletInstance.GetComponent<Bullet>().SetColour(bulletColour);
+            ammo--;
+            bulletColour++;
+        }
+        else
+        {
+            Debug.Log("Empty!");
+        }
+    }
+
+    //Resets the player ammo loaded if reloads are avaiable
+    //Resets the color counter
+    void HandleAmmo()
+    {
+        if (ammo <= 0 && reloads > 0)
+        {
+                ammo = 4;
+        }
+
+        if (bulletColour >= 4)
+        {
+            bulletColour = 0;
+        }
+    }
+}
