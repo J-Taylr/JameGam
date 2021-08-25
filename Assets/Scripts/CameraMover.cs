72515db0e0c85b84d3255b78ff7634771e64269b
J-Taylor <jmarcustaylor@gmail.com>
2021-08-25 11:39:07 +0100

    Initial commit + .gitignore

---
+++ Assets/Scripts/CameraMover.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMover : MonoBehaviour
+{
+    public List<Transform> camPositions = new List<Transform>();
+  
+
+    private void Awake()
+    {
+    }
+
+   
+
+    void Start()
+    {
+        transform.position = camPositions[0].position;
+       
+    }
+
+    public void TransitionRoom(int roomNumber)
+    {
+       
+       
+        if (camPositions[roomNumber] != null)
+        {
+            transform.position = camPositions[roomNumber].position;
+            
+        }
+    }
+   
+}
---
+++ Assets/Scripts/RoomIdentifier.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomIdentifier : MonoBehaviour
+{
+    CameraMover cameraMover;
+    BoxCollider2D col;
+
+    public int roomNumber = 1;
+
+    private void Awake()
+    {
+        cameraMover = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<CameraMover>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+
+            
+            print("transitioning to " + roomNumber);
+            cameraMover.TransitionRoom(roomNumber);
+
+        }
+    }
+
+   
+   
+    
+}
